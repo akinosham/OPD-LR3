3131cb560fea1b93fae148934e0d0a88d48e8ed3
akinosham <veronika.mashkina20@gmail.com>
2024-10-15 14:37:25 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 from flask import Flask, render_template, request
 import math
+
 app = Flask(__name__)
 
-result = 0
-def calculate(func, value, precision):
+def calculate_trigonometry(value, func, precision):
     if func == "sin":
         result = math.sin(math.radians(value)) if precision == "degrees" else math.sin(value)
-    if func == "cos":
+    elif func == "cos":
         result = math.cos(math.radians(value)) if precision == "degrees" else math.cos(value)
-    if func == "tan":
+    elif func == "tan":
         result = math.tan(math.radians(value)) if precision == "degrees" else math.tan(value)
-    if func == "ctan":
-        result = math.tan(math.radians(value)) if precision == "degrees" else math.tan(value)
-        result = 1/result
     return round(result, 4)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    if request.method == "POST":
+    if request.method == 'POST':
         value = float(request.form['value'])
         func = request.form['func']
         precision = request.form['precision']
-        result = calculate(func, value, precision)
+        result = calculate_trigonometry(value, func, precision)
         return render_template('result.html', result=result)
     return render_template('index.html')
 
